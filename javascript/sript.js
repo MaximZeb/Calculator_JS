6d0bea28c916f1cb7241c68a6efa82fe6be1b0f8
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-01-03 20:30:49 +0500

    create calculator

---
+++ javascript/sript.js
@@ -0,0 +1,25 @@
+
+// прикрепляем кнопки с их значением 
+function insert (num){
+	document.form.textview.value = 
+	document.form.textview.value + num;
+}
+
+// очистка input
+function clean () {
+	document.form.textview.value = "";
+}
+
+// удаляет последнию цифру
+function back () {
+	var public = document.form.textview.value;
+	document.form.textview.value = public.substring(0,public.length-1);
+}
+
+// подсчет выражения 
+function equal () {
+	var public = document.form.textview.value;
+	if(public) {
+		document.form.textview.value = eval(public);
+	}
+}
